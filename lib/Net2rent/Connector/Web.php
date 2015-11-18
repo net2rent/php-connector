80219c863f45bd5e8d8ea38064597b520b304518
jordigarciad <jgarcia@rourets.com>
2015-11-18 11:12:43 +0100

    Add filter to getRates

--- lib/Net2rent/Connector/Web.php
+++ lib/Net2rent/Connector/Web.php
@@ -128,6 +128,7 @@ class Web extends AbstractConnector
         } else {
             $params['only_contract']=0;
         }
+        $params['orderby']='seasondate.day';
         
         $seasonsRates=array();
         
