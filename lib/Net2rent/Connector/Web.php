38a48e9399b16f24f8c9749d5dd204f2d62b901b
jordigarciad <jordigarciad@gmail.com>
2016-05-25 19:52:49 +0200

    Add new parameters to getProperty. Move getPropertyAccessories from Web to Abstract

--- lib/Net2rent/Connector/Web.php
+++ lib/Net2rent/Connector/Web.php
@@ -279,73 +279,7 @@ class Web extends AbstractConnector
             
         return $seasonsOverwrittenRates;
     }
-    
-    /**
-     * Gets property availability and prices
-     *
-     * @param  string  $propertyId
-     * @param  date  $options['mandatory'] Filter by mandatory (1=yes, 0=no)
-     * @param  lg    $options['mandatory'] Language of the accessory name (values: ca,es,en,fr,de,nl,ru,it)
-     * @param  date  $options['from'] First date to get availability and prices. Format YYYY-MM-DD. Optional, if not set gets one year
-     * @param  date  $options['to'] Last date to get availability and prices. Format YYYY-MM-DD. Optional, if not set gets one year
-     * @return array  (items)
-     */
-    public function getPropertyAccessories($propertyId, array $options = array())
-    {
-        $endPoint = $this->getEndPoint('property_accessories', array(
-            $propertyId
-        ));
-
-        $params = array();
-        $params['lg']=isset($options['lg']) && $options['lg'] ?  $options['lg'] : ""; 
         
-        $mandatory=isset($options['mandatory']) ? $options['mandatory'] : null; 
-        $checkin=isset($options['checkin']) && $options['checkin'] ? $options['checkin'] : null;   
-        $checkout=isset($options['checkout']) && $options['checkout'] ? $options['checkout'] : null;   
-        
-        // calculate night number
-        $date_in_datetime=date_create($checkin);
-        $date_out_datetime=date_create($checkout);        
-        $night_number =(int)date_diff($date_in_datetime,$date_out_datetime)->format('%a');
-        
-        $accessories = $this->api(sprintf($endPoint . '?%s', http_build_query($params)));
-        $accessoriesReturn=array();
-        
-        foreach($accessories as $accessory) {
-            // ignore non public accessories
-            if(isset($accessory['public']) && !(int)$accessory['public']) {
-                continue;
-            }
-          
-            if(isset($mandatory)) {
-                // ignore accessories with mandatory distinct from the submitted option
-                if($mandatory!=(int)$accessory['mandatory']) {
-                    continue;
-                }
-                
-                // if accessory is mandatory and checkin is not between dates, ignore
-                if($accessory['mandatory']==1 && $checkin && $accessory['date_from'] && $checkin<$accessory['date_from']) {
-                    continue;
-                }
-                
-                if($accessory['mandatory']==1 && $checkin && $accessory['date_to'] && $checkin>$accessory['date_to']) {
-                    continue;
-                }
-            }
-            
-            // calculate accessory price
-            $totalprice=0;
-            if($accessory['price_type']=='day') { $totalprice=$accessory['price']*$night_number; }
-            else if($accessory['price_type']=='week') { $totalprice=($accessory['price']/7)*$night_number; }
-            else { $totalprice=$accessory['price']; }
-            $accessory['totalprice']=$totalprice;
-            
-            $accessoriesReturn[]=$accessory;
-        }
-        
-        return $accessoriesReturn;
-    }
-    
     /**
      * Gets contacts from a company
      *
